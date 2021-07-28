1b8e9f26b73586cadffbe6378420a99730f8b45d
Bruno Geraldi <brunogeraldieng@gmail.com>
2021-07-27 21:52:11 -0300

    Mobile CSS, HTML and JS done, desktop in progress

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 let emailDiv = document.getElementById("email-div")
 let emailInput = document.getElementById("email-input")
+let button = document.getElementById("button")
+let errorImg = document.getElementById("error")
 
 function emailDivFocus() {
     
-    emailDiv.classList.toggle("email-div-error")
+    emailDiv.classList.toggle("email-div-focus")
     
 }
 
 function emailDivError() {
-    emailDiv.classList.toggle("email-div-error")
+    let successMsg = document.getElementById("success-msg")
+    let errorMsg = document.getElementById("error-msg")
+
+    let email = emailInput.value
+
+    let containsAt = email.includes("@")
+    let splitEmail = email.split("@")
+
+    if (containsAt && splitEmail[1].length != 0 && splitEmail.length == 2) {
+        successMsg.style.display = "block"
+        errorMsg.style.display = "none"
+        errorImg.style.display = "none"
+        
+        emailDiv.classList.remove("email-div-error")
+        
+    } else {
+        successMsg.style.display = "none"
+        errorMsg.style.display = "block"
+        errorImg.style.display = "block"
+
+        emailDiv.classList.add("email-div-error")
+
+
+    }
+    console.log(splitEmail.length)
+    // emailDiv.classList.toggle("email-div-error")
 }
 
 emailInput.addEventListener("focus", emailDivFocus)
 emailInput.addEventListener("focusout", emailDivFocus)
+
+button.addEventListener("click", emailDivError)
